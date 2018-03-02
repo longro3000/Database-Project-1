74661a9f6e06014edbcacae760490871856ddd8f
longro3000 <longro3000@gmail.com>
2018-03-02 06:08:28 +0200

    upload run.bat and setup.bat

---
+++ setup.bat
@@ -0,0 +1,10 @@
+echo off
+set PATH=%PATH%;Y:\staff\_tka\PortableGit\bin;Y:\staff\_tka\python;Y:\staff\_tka\python\Scripts
+echo python and git are now available
+git clone -b Project-e1601108 https://github.com/longro3000/Database-Project-1.git
+echo Project's been cloned to this folder
+python -m pip install djangorestframework-xml
+python -m pip install djangorestframework-jsonapi
+echo all necessary framework's been installed
+echo installation completed
+python manage.py createsuperuser
